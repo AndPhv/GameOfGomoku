6051abaed7653511f3906e5077b72c47008de732
AndPhv <puhov2001@gmail.com>
2022-03-26 18:30:23 +0300

    Создание класса Cell,  переменных rowIndex и colIndex и конструктора

---
+++ src/com/company/Interfaces/Cell.java
@@ -0,0 +1,11 @@
+package com.company.Interfaces;
+
+public class Cell {
+    private final int rowIndex;
+    private final int colIndex;
+
+    public Cell (int rowIndex, int colIndex) {
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+    }
+}
