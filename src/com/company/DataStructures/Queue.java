0e3cdc816a02207a45bfd8a0eed918a8f3bce3a9
AndPhv <puhov2001@gmail.com>
2022-03-29 20:00:01 +0300

    Создание Queue и реализация методов из DataStructure

---
+++ src/com/company/DataStructures/Queue.java
@@ -0,0 +1,28 @@
+package com.company.DataStructures;
+
+public class  Queue<T> extends DataStructure<T> {
+    public Queue() {
+        super(new LinkedList<T>());
+    }
+
+    protected Queue(DataSet<T> dataSet) {
+        super(dataSet);
+    }
+
+    @Override
+    protected int getCurrentIndex() {
+        return 0;
+    }
+
+    @Override
+    protected RuntimeException createEmptyExceptionInstance() {
+        return new QueueIsEmptyException();
+    }
+
+    public static class QueueIsEmptyException extends RuntimeException {
+        private static final long serialVersionUID = 5611538616921447536L;
+        private QueueIsEmptyException(){
+            super("Current queue is empty");
+        }
+    }
+}
