7bdcdaacf903983487214be1ab80d7bf181449b2
AndPhv <puhov2001@gmail.com>
2022-04-07 13:19:53 +0300

    Создание объекта GUIGomoku в методе main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import com.company.Interfaces.GUIGomoku;
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-
     public static void main(String[] args) {
-
+        GUIGomoku w = new GUIGomoku();
+        w.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        w.setResizable(false);
+        w.pack();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        w.setLocation(dim.width / 2 - w.getSize().width / 2, dim.height / 2 - w.getSize().height / 2);
+        w.setVisible(true);
     }
 }
